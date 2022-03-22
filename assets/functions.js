48810f0226c76a1364c851970fa12b5581bd74da
sarvarsangilov <srvrsangilov@gmail.com>
2022-03-22 18:34:24 +0500

    Update

---
+++ assets/functions.js
@@ -0,0 +1,192 @@
+Amplitude.init({
+    bindings: {
+      37: 'prev',
+      39: 'next',
+      32: 'play_pause'
+    },
+    debug: true,
+    visualization: 'michaelbromley_visualization',
+    songs: [
+      {
+        "name": "ФИКСЕТТА",
+        "artist": "FixEye",
+        "album": "YouTube",
+        "url": "/audio/Фиксай_ФИКСЕТТА_Официальный_клип.mp3",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+        "visualization": "michaelbromley_visualization"
+      },
+      {
+        "name": "JUMANJI",
+        "artist": "HEDEGAARD_feat",
+        "album": "Unknow",
+        "url": "/audio/HEDEGAARD_feat._CANCUN -JUMANJI.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Звёзды",
+        "artist": "Yellow fire",
+        "album": "YouTube",
+        "url": "/audio/audio.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Mir",
+        "artist": "Raym",
+        "album": "YouTube",
+        "url": "/audio/raym-mir.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Не играй со мной",
+        "artist": "YUNI",
+        "album": "Youtube",
+        "url": "/audio/yuni.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Элитры",
+        "artist": "Yellow Fire 2022",
+        "album": "Minecraft Клип-YouTube",
+        "url": "/audio/Элитры - Minecraft Клип - Yellow Fire 2022.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Super Sako",
+        "artist": "Mi GNA",
+        "album": "Remix Bass Boosted",
+        "url": "/audio/SUPER SAKO Mi GNA Remix Bass Boosted   Unknown artist.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Пчела упала",
+        "artist": "Yellow Fire",
+        "album": "YouTube",
+        "url": "/audio/videoplayback (3).mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Tы меня предала",
+        "artist": "Yellow Fire",
+        "album": "YouTube",
+        "url": "/audio/videoplayback (4).mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Юность",
+        "artist": "Darba",
+        "album": "Youtube",
+        "url": "/audio/dabro_yunost_premera_pesni_2020_zvuk_postavim_na_vsyu_muztea_net.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Костяная рука ",
+        "artist": "W Labs",
+        "album": "YouTube",
+        "url": "/audio/videoplayback~1.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Barmen",
+        "artist": "Soul, Руслан Добрый",
+        "album": "Unknow",
+        "url": "/audio/barmen.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Detsvo",
+        "artist": "Rauf Fike",
+        "album": "Youtube",
+        "url": "/audio/Rauf_Faik_-_Detstvo.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Stasafiya",
+        "artist": "Imran Khan",
+        "album": "YouTube",
+        "url": "/audio/Satisfaya.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Molniya",
+        "artist": "Dima Bilan",
+        "album": "Youtube",
+        "url": "/audio/Dima_Bilan_-_Molniya.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Детка ламбада",
+        "artist": "MiyaGi",
+        "album": "Youtube",
+        "url": "/audio/MiyaGi- Детка ламбада   Неизвестный.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Vali",
+        "artist": "FixEye",
+        "album": "Youtube",
+        "url": "/audio/vali.mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      },
+      {
+        "name": "Будь тише",
+        "artist": "Yellow Fire",
+        "album": "Youtube",
+        "url": "/audio/videoplayback (2).mp3",
+        "visualization": "michaelbromley_visualization",
+        "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+      }
+    ],
+  
+    waveforms: {
+        sample_rate: 50
+    },
+  
+    visualizations: [
+      {
+        object: MichaelBromleyVisualization,
+        params: {
+  
+        }
+      }
+    ]
+});
+  
+
+$(function() {
+  var rangePercent = $('[type="range"].amplitude-volume-slider').val();
+  rangePercent = $('[type="range"].amplitude-volume-slider').val();
+    $('h4').html(rangePercent+'<span class="valume"></span>');
+    $('[type="range"].amplitude-volume-slider, h4>span.valume').css('filter', 'hue-rotate(-' + rangePercent + 'deg)');
+    $('h4').css({'transform': 'translateX(-50%) '/*scale(' + (1+(rangePercent/100)) + ')'*/, 'left': rangePercent+'%'});
+  $('[type="range"].amplitude-volume-slider').on('change input', function() {
+      rangePercent = $('[type="range"].amplitude-volume-slider').val();
+      $('h4').html(rangePercent+'<span class="valume"></span>');
+      $('[type="range"].amplitude-volume-slider, h4>span.valume').css('filter', 'hue-rotate(-' + rangePercent + 'deg)');
+      $('h4').css({'transform': 'translateX(-50%) '/*scale(' + (1+(rangePercent/100)) + ')'*/, 'left': rangePercent+'%'});
+  });
+});
+
+function playlist_open() {
+  var elem = document.getElementById("white-player-playlist-container"),
+      elem2 = document.getElementById("white-player");
+
+  elem2.classList.toggle("close");
+  elem.classList.toggle("open");
+}
+
